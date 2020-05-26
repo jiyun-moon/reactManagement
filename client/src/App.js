faae640cb17c01b7a7a5235b3880875d2f71e024
likemoonji <jiyun0527@gmail.com>
2020-05-26 13:24:57 +0900

    add loadding bar

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -18,15 +19,20 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing * 2
   }
 });
 
 class App extends Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err))
@@ -37,6 +43,11 @@ class App extends Component {
     const body = await response.json();
     console.log('resdata', body)
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   }
 
 
@@ -66,7 +77,13 @@ class App extends Component {
                 gender={user.gender}
                 job={user.job}
               />
-            }) : ""}
+            }) :
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
